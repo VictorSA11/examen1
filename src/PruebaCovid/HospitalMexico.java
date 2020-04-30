1e2e341075c0a35e0e04b06cf787eed63beca07e
Victor Segura <viko_1995@2hotmail.com>
2020-04-29 21:51:06 -0600

    commit

---
+++ src/PruebaCovid/HospitalMexico.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PruebaCovid;
+
+import java.util.List;
+
+/**
+ *
+ * @author Victor Segura
+ */
+public class HospitalMexico extends OMS{
+
+    public HospitalMexico() {
+    }
+
+    @Override
+    protected boolean isGamHospital() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Hospital getLugar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
+    }
+    
+}
