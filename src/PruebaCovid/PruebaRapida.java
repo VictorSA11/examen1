75d4de5543dd9a983d689ceaf221e9b9a4cdcf5f
Victor Segura <viko_1995@2hotmail.com>
2020-04-29 21:12:58 -0600

    commit

---
+++ src/PruebaCovid/PruebaRapida.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PruebaCovid;
+
+/**
+ *
+ * @author Victor Segura
+ */
+public class PruebaRapida {
+
+    private final Paciente pac;
+
+    public PruebaRapida(Paciente paciente) {
+   
+    this.pac=paciente;
+    }
+    
+}
